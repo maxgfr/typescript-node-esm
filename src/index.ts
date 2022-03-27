2f0ecb6bb825eb0e554fe29a6d17e59a257c9f38
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-27 13:07:45 +0200

    fix: esm

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config'; // To use our .env
 import fetch from 'node-fetch'; // pure esm lib
+import { sayHello } from './hello';
 
 async function main () {
+  sayHello();
   const response = await fetch('https://rickandmortyapi.com/api/character');
   const data = await response.json();
   console.log(data)
-  console.log("Envrionment variable", process.env.MY_VARIABLE);
 }
 
 main();
